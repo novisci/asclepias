bc43c7e38788111443f04f6c5f727b08d579efe3
bsaul <bradleysaul@gmail.com>
2019-07-26 11:05:52 -0400

    a first pass at defining the Context type

--- hasklepias/Hasklepias/Context.hs
+++ hasklepias/Hasklepias/Context.hs
@@ -1,24 +1,88 @@
 module Hasklepias.Context(
-  Context,
-  -- CardSuitContext,
-  cardSuit
+  --Context,
+  --CardSuitContext,
+  card,
+  cardContext
 ) where
 
 import Prelude hiding (lookup)
-
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Context
-
-import Data.IntMap (IntMap)
-import qualified Data.IntMap.Strict as ContextMap
-
-type Context a b = Context.Map String( Context.Map a b )
-
-type CardSuit = Context.Map String String
-
-cardSuit :: String -> Maybe CardSuit
-cardSuit s 
-   | s `elem` ["spades", "diamonds", "hearts", "club"] = Just (Context.fromList [("suit", s)])
-   | otherwise = Nothing
+import qualified Data.Map.Strict as M
 
 
+-- | TODO: define context and its purpose
+--   key = Maybe value, 
+newtype Context a b = Context (M.Map a (Maybe b))
+ deriving (Eq, Ord, Show)
+
+
+
+{-
+  Context Examples
+-}
+
+data Game =
+      Poker
+    | BlackJack
+    | War
+    deriving (Show)
+
+data Suit = 
+     Spades 
+   | Diamonds 
+   | Hearts
+   | Clubs 
+
+instance Show Suit where
+   show Spades   = "\9824"
+   show Diamonds = "\9830"
+   show Hearts   = "\9829"
+   show Clubs    = "\9827"
+
+data Rank = 
+     Ace 
+   | Two 
+   | Three
+   | Four
+   | Five 
+   | Six 
+   | Seven 
+   | Eight 
+   | Nine 
+   | Ten 
+   | Jack 
+   | Queen 
+   | King
+
+instance Show Rank where
+    show Ace   = "A" 
+    show Two   = "2" 
+    show Three = "3"
+    show Four  = "4"
+    show Five  = "5" 
+    show Six   = "6"
+    show Seven = "7"
+    show Eight = "8"
+    show Nine  = "9"
+    show Ten   = "10"
+    show Jack  = "J"
+    show Queen = "Q"
+    show King  = "K"
+
+
+data Card = Card { 
+     suit :: Suit
+   , rank :: Rank }
+
+instance Show Card where
+  show x = (show $ rank x) ++ (show $ suit x)
+
+--type PokerHand = Context (M.Map Suit Rank)
+--type CardContext = Context (M.Map Game Card)
+type CardContext = Context Game Card
+
+card :: Suit -> Rank -> Card
+card s v = Card {suit = s, rank = v}
+
+cardContext :: Game -> Maybe Card -> CardContext
+cardContext g (Just c) = Context $ M.singleton g (Just c)
+cardContext g Nothing  = Context $ M.singleton g Nothing

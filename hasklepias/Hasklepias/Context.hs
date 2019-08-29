58950a04e2baa9eedd1acecbce33fc6f50f6b95d
bsaul <bradleysaul@gmail.com>
2019-08-29 09:57:38 -0400

    adds Source type to contexts; adds a draft example JSON data file

--- hasklepias/Hasklepias/Context.hs
+++ hasklepias/Hasklepias/Context.hs
@@ -1,21 +1,33 @@
 module Hasklepias.Context(
     Context
   , context
+  , contextns
   , getDomain
   , getInfo
+  , getSource
 ) where
 
+import Prelude hiding (lookup)
+import qualified Data.Map.Strict as M
 
+-- | TODO: define Source and its purpose
 
--- | TODO: define context and its purpose
---   key = Maybe value, 
---newtype Context a = Context { getContext :: (M.Map String (Maybe a)) }
--- deriving (Eq, Ord, Show)
+type Source = Maybe (M.Map String String)
+
+-- | TODO: define Context and its purpose
 
 data Context a = Context {
       getDomain :: String
-    , getInfo   :: a }
+    , getInfo   :: a 
+    , getSource :: Source}
     deriving (Eq, Show) 
 
-context :: String -> a -> Context a
-context d i = Context d i
+-- | Smart contructor for Context type
+
+context :: String -> a -> Source -> Context a
+context d i s = Context d i s
+
+-- | Smart contructor for Context type without a Source
+
+contextns :: String -> a -> Context a
+contextns d i = context d i Nothing

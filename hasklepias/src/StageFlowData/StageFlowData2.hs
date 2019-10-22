b3231f0ead1cf3e01c2d0e59576aa71b4cc26155
bsaul <bradleysaul@gmail.com>
2019-10-22 13:23:59 -0400

    updates disjointEvents to handle case of same state; othe clean up

--- hasklepias/src/StageFlowData/StageFlowData2.hs
+++ hasklepias/src/StageFlowData/StageFlowData2.hs
@@ -7,18 +7,14 @@ import Hasklepias.IntervalAlgebra
 import qualified Data.ByteString.Lazy as B
 import Data.Aeson
 import Data.Aeson.Types
-import Data.List (nub, sort)
+import Data.List (nub)
 import Data.Text.Lazy (Text)
 import qualified Data.Text.Lazy.IO as I
 import Data.Aeson.Text (encodeToLazyText)
 import Text.Printf
 import System.CPUTime
 
-s1 = Events [
-    Event (period 0 3,  2)
-  , Event (period 5 8,  2)
-  , Event (period 5 10, 0)
-  , Event (period 8 12, 4)]
+--- Data types ---
 
 type State = Int
 
@@ -29,25 +25,6 @@ makeState _   = 0
 
 none :: State
 none = 0
-
---type Event = (Period, State)
-
-
-
-h :: ([Event], [Event]) -> [Event] -> ([Event], [Event])
-h (c, o:os) []     = (c++o:os, [])
-h (c, [])   (e:es) = h (c, [e]) es
-h (c, o:os) (e:es) 
-  | p1 `before` p2 || p1 `meets` p2 = h (c++nh, (fst $ h ([], nt) os )) es
-  | e == o           = h (c, o:os) es
-  | otherwise        = h (c, (fst $ h ([], n) os )) es
-  where n  = disjointEvents e1 e2
-        e1 = min o e
-        e2 = max o e
-        nh = [head n]
-        nt = tail n
-        p2 = fst $ unEvent e
-        p1 = fst $ unEvent o
 
 newtype Event = Event { unEvent :: (Period, State) }
   deriving (Eq, Show)
@@ -72,6 +49,9 @@ instance FromJSON Event where
 event :: Period -> State -> Event
 event p s = Event (p, s)
 
+eventp :: Int -> Int -> State -> Event
+eventp b e s = event (period b e) s
+
 newtype Events = Events { unEvents :: [Event] } deriving (Eq, Show)
 
 instance FromJSON Events where
@@ -81,27 +61,41 @@ instance FromJSON Events where
 
 --- Processing ---
 
--- | Assuming e1 <= e2, there are 7 possible relations between e1 and e2.
---   NOTE: This currently assumes that all contained, overlapping or meeting 
---   periods of the same state have been collapsed.
-
-newEvents :: [Event] -> [Int]
-newEvents e = [(begin $ fst $ unEvent $ head e) .. (end $ fst $ unEvent $ last e)  ]
+h :: ([Event], [Event]) -> [Event] -> ([Event], [Event])
+h (c, o:os) []     = (c++o:os, [])
+h (c, [])   (e:es) = h (c, [e]) es
+h (c, o:os) (e:es) 
+  | p1 `before` p2 || 
+    p1 `meets`  p2   = h (c++nh, (fst $ h ([], nt) os )) es
+  | e == o           = h (c, o:os) es
+  | otherwise        = h (c, (fst $ h ([], n) os )) es
+  where n  = disjointEvents e1 e2
+        e1 = min o e
+        e2 = max o e
+        nh = [head n]
+        nt = tail n
+        p2 = fst $ unEvent e
+        p1 = fst $ unEvent o
 
 combineStates :: State -> State -> State
 combineStates s1 s2
    | s1 == s2  = s1
    | otherwise = s1 + s2
 
+-- | Assuming e1 <= e2, there are 7 possible relations between e1 and e2.
+--   NOTE: This currently assumes that all contained, overlapping or meeting 
+--   periods of the same state have been collapsed.
+
 disjointEvents :: Event -> Event -> [Event]
 disjointEvents e1 e2 
-   | p1 == p2           = [event p1 s3]
-   | p1 `starts` p2     = [event p1 s3, event (period p1e p2e) s2 ]
-   | p1 `finishedBy` p2 = [event (period p1b p2b) s1, event p2 s3]
-   | p1 `contains` p2   = [event (period p1b p2b) s1, event p2 s3, event (period p2e p1e) s1]
-   | p1 `meets` p2      = [e1, e2]
-   | p1 `overlaps` p2   = [event (period p1b p2b) s1, event (period p2b p1e) s3, event (period p1e p2e) s2]
-   | p1 `before` p2     = [e1, event (period p1e p2b) none, e2]
+   |           p1 == p2           = [event p1 s3]
+   |           p1 `before` p2     = [e1, eventp p1e p2b none, e2]
+   | not sS && p1 `starts` p2     = [event p1 s3, eventp p1e p2e s2 ]
+   | not sS && p1 `finishedBy` p2 = [eventp p1b p2b s1, event p2 s3]
+   | not sS && p1 `contains` p2   = [eventp p1b p2b s1, event p2 s3, eventp p2e p1e s1]
+   | not sS && p1 `meets` p2      = [e1, e2]
+   | not sS && p1 `overlaps` p2   = [eventp p1b p2b s1, eventp p2b p1e s3, eventp p1e p2e s2]
+   | otherwise                    = [eventp p1b p2e s1]      
    where p1  = fst $ unEvent e1
          p2  = fst $ unEvent e2
          s1  = snd $ unEvent e1
@@ -111,8 +105,21 @@ disjointEvents e1 e2
          p2b = begin p2
          p1e = end p1
          p2e = end p2
+         sS  = s1 == s2
 
+--- Testing ---
 
+s1 =
+  [ eventp 0 3 2
+  , eventp 5 8 2
+  , eventp 5 10 1
+  , eventp 8 12 4]
+
+s2 =
+  [ eventp 0 3 2
+  , eventp 5 8 2
+  , eventp 5 10 1
+  , eventp 8 12 1]
 
 --- IO ---
 

c68f5672168cb6ac66d6ad98d75e0594bc0f5769
bsaul <bradleysaul@gmail.com>
2019-10-22 12:50:38 -0400

    gets a working function up and running

--- hasklepias/src/StageFlowData/StageFlowData2.hs
+++ hasklepias/src/StageFlowData/StageFlowData2.hs
@@ -32,44 +32,22 @@ none = 0
 
 --type Event = (Period, State)
 
-data Tree a = 
-    Nada
-  | Node a (Tree a) (Tree a) 
-  deriving (Eq, Show)
 
-type EventTree = Tree Event
-
-
-nTree :: a -> Tree a
-nTree x = Node x Nada Nada
-
-
-f :: Event -> EventTree
-f e = Node e (Nada) (Nada)
-
-g :: Event -> EventTree -> EventTree
-g e (Node x _ _) = disjointEvents' x e
-g e Nada = nTree e
-
-safeTail :: [a] -> [a]
-safeTail [] = []
-safeTail (x:xs) = xs
 
 h :: ([Event], [Event]) -> [Event] -> ([Event], [Event])
-h (c, o:os) []        = (c++o:os, [])
+h (c, o:os) []     = (c++o:os, [])
 h (c, [])   (e:es) = h (c, [e]) es
 h (c, o:os) (e:es) 
-  | p2 `after` p1  = h (c++nh, (fst $ h ([], nt) os )) es
-  | p2 `metBy` p1  = h (c++nh, (fst $ h ([], nt) os )) es
-  | e == o         = h (c, o:os) es
-  | otherwise      = h (c, (fst $ h ([], n) os )) es
---  | otherwise      = h (c, n) es
-  where n  = disjointEvents o e
+  | p1 `before` p2 || p1 `meets` p2 = h (c++nh, (fst $ h ([], nt) os )) es
+  | e == o           = h (c, o:os) es
+  | otherwise        = h (c, (fst $ h ([], n) os )) es
+  where n  = disjointEvents e1 e2
+        e1 = min o e
+        e2 = max o e
         nh = [head n]
         nt = tail n
         p2 = fst $ unEvent e
         p1 = fst $ unEvent o
- 
 
 newtype Event = Event { unEvent :: (Period, State) }
   deriving (Eq, Show)
@@ -110,30 +88,10 @@ instance FromJSON Events where
 newEvents :: [Event] -> [Int]
 newEvents e = [(begin $ fst $ unEvent $ head e) .. (end $ fst $ unEvent $ last e)  ]
 
-
 combineStates :: State -> State -> State
 combineStates s1 s2
    | s1 == s2  = s1
    | otherwise = s1 + s2
-
-disjointEvents' :: Event -> Event -> EventTree
-disjointEvents' e1 e2 
-   | p1 == p2           = nTree (event p1 s3)
-   | p1 `starts` p2     = Node (event p1 s3) Nada (nTree (event (period p1e p2e) s2))
-   | p1 `finishedBy` p2 = Node (event (period p1b p2b) s1) Nada (nTree (event p2 s3))
-   | p1 `contains` p2   = Node (event p2 s3) (nTree (event (period p1b p2b) s1)) (nTree (event (period p2e p1e) s1))
-   | p1 `meets` p2      = Node e1 Nada (nTree e2)
-   | p1 `overlaps` p2   = Node (event (period p2b p1e) s3) (nTree (event (period p1b p2b) s1)) (nTree (event (period p1e p2e) s2))
-   | p1 `before` p2     = Node (event (period p1e p2b) none) (nTree e1) (nTree e2)
-   where p1  = fst $ unEvent e1
-         p2  = fst $ unEvent e2
-         s1  = snd $ unEvent e1
-         s2  = snd $ unEvent e2
-         s3  = combineStates s1 s2
-         p1b = begin p1
-         p2b = begin p2
-         p1e = end p1
-         p2e = end p2
 
 disjointEvents :: Event -> Event -> [Event]
 disjointEvents e1 e2 
@@ -154,16 +112,6 @@ disjointEvents e1 e2
          p1e = end p1
          p2e = end p2
 
-type Closed = [Event]
-type Open   = [Event]
-type Clopen = (Closed, Open)
-
-{-
-h :: Clopen -> Event -> [[Event]]
-h c e2 
- | empty snd c = 
- | map (\e1 -> disjointEvents e1 e2 ) $ snd c 
--}
 
 
 --- IO ---

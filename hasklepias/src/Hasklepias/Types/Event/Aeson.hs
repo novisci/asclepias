b5a1d77acb464bf0ac9961d79beeaa1064c91d3b
bsaul <bradleysaul@gmail.com>
2020-07-15 10:27:39 -0400

    adds an initial parser for Day events

--- hasklepias/src/Hasklepias/Types/Event/Aeson.hs
+++ hasklepias/src/Hasklepias/Types/Event/Aeson.hs
@@ -12,12 +12,14 @@ module Hasklepias.Types.Event.Aeson(
       Context(..)
     , Event(..)
     , parseEventIntLines
+    , parseEventDayLines
 ) where
 
 import IntervalAlgebra
 import Hasklepias.Types.Context
 import Hasklepias.Types.Event
 import Data.Aeson
+import Data.Time
 import Data.Vector ((!))
 import qualified Data.ByteString.Lazy as B
 import qualified Data.ByteString.Char8 as C
@@ -30,10 +32,22 @@ instance FromJSON (Interval Int) where
         e <- t .: "end"
         return (unsafeInterval (b :: Int) (e :: Int))
 
+instance FromJSON (Interval Day) where
+    parseJSON = withObject "Time" $ \o -> do
+        t <- o .: "time"
+        b <- t .: "begin"
+        e <- t .: "end"
+        return (unsafeInterval (b :: Day) (e :: Day))
+
 instance FromJSON Context where
     parseJSON v = context <$> (parseJSON v)
 
 instance FromJSON (Event Int) where
+    parseJSON (Array v) = event <$>
+            (parseJSON (v ! 5)) <*>
+            (parseJSON (v ! 4))
+
+instance FromJSON (Event Day) where
     parseJSON (Array v) = event <$>
             (parseJSON (v ! 5)) <*>
             (parseJSON (v ! 4))
@@ -46,3 +60,7 @@ parseEventIntLines l =
     rights $ map (\x -> eitherDecode $ B.fromStrict x :: Either String (Event Int)) 
         (C.lines $ B.toStrict l)
 
+parseEventDayLines :: B.ByteString -> [Event Day]
+parseEventDayLines l = 
+    rights $ map (\x -> eitherDecode $ B.fromStrict x :: Either String (Event Day)) 
+        (C.lines $ B.toStrict l)
--- hasklepias/test/Hasklepias/Types/Event/AesonSpec.hs
+++ hasklepias/test/Hasklepias/Types/Event/AesonSpec.hs
@@ -5,17 +5,18 @@ import IntervalAlgebra
 import Hasklepias.Types.Event
 import Hasklepias.Types.Event.Aeson
 import Data.Aeson
+import Data.Time as DT
 import Hasklepias.Types.Context as HC
 import Test.Hspec
 import qualified Data.ByteString.Lazy as B
 
-testIn :: B.ByteString
-testIn = "[\"abc\", 0, 1, \"Diagnosis\",\
+testInInt :: B.ByteString
+testInInt = "[\"abc\", 0, 1, \"Diagnosis\",\
           \[\"someThing\"],\
           \{\"domain\":\"Diagnosis\",\"time\":{\"begin\":0,\"end\":1}}]"
 
-testInputs :: B.ByteString
-testInputs = 
+testInputsInt :: B.ByteString
+testInputsInt = 
       "[\"abc\", 0, 1, \"Diagnosis\",\
       \[\"someThing\"],\
       \{\"domain\":\"Diagnosis\",\"time\":{\"begin\":0,\"end\":1}}]\n\
@@ -23,13 +24,39 @@ testInputs =
       \[\"someThing\"],\
       \{\"domain\":\"Diagnosis\",\"time\":{\"begin\":5,\"end\":6}}]"
 
+testInDay :: B.ByteString
+testInDay = "[\"abc\", \"2020-01-01\", \"2020-01-02\", \"Diagnosis\",\
+          \[\"someThing\"],\
+          \{\"domain\":\"Diagnosis\",\
+          \ \"time\":{\"begin\":\"2020-01-01\",\"end\":\"2020-01-02\"}}]"
 
-testOut1 = event (unsafeInterval (0 :: Int) (1 :: Int)) (HC.context ["someThing"])
-testOut2 = event (unsafeInterval (5 :: Int) (6 :: Int)) (HC.context ["someThing"])
+
+testInputsDay :: B.ByteString
+testInputsDay = 
+      "[\"abc\", \"2020-01-01\", \"2020-01-02\", \"Diagnosis\",\
+      \[\"someThing\"],\
+      \{\"domain\":\"Diagnosis\",\
+      \ \"time\":{\"begin\":\"2020-01-01\",\"end\":\"2020-01-02\"}}]\n\
+      \[\"abc\", \"2020-01-05\", \"2020-01-06\", \"Diagnosis\",\
+      \[\"someThing\"],\
+      \{\"domain\":\"Diagnosis\",\
+      \ \"time\":{\"begin\":\"2020-01-05\",\"end\":\"2020-01-06\"}}]"
+
+testOutInt1 = event (unsafeInterval (0 :: Int) (1 :: Int)) (HC.context ["someThing"])
+testOutInt2 = event (unsafeInterval (5 :: Int) (6 :: Int)) (HC.context ["someThing"])
+
+testOutDay1 = event (unsafeInterval (fromGregorian 2020 1 1) (fromGregorian 2020 1 2)) (HC.context ["someThing"])
+testOutDay2 = event (unsafeInterval (fromGregorian 2020 1 5) (fromGregorian 2020 1 6)) (HC.context ["someThing"])
+
 
 spec :: Spec 
 spec = do 
-    it "an event is parsed correctly" $ 
-       ( decode testIn )  `shouldBe` (Just testOut1)
-    it "lines of events are parsed correctly" $
-       (parseEventIntLines testInputs) `shouldBe` [testOut1, testOut2]
+    it "an Int event is parsed correctly" $ 
+       ( decode testInInt )  `shouldBe` (Just testOutInt1)
+    it "lines of Int events are parsed correctly" $
+       (parseEventIntLines testInputsInt) `shouldBe` [testOutInt1, testOutInt2]
+
+    it "a Day event is parsed correctly" $ 
+       ( decode testInDay )  `shouldBe` (Just testOutDay1)
+    it "lines of Int events are parsed correctly" $
+       (parseEventDayLines testInputsDay) `shouldBe` [testOutDay1, testOutDay2]

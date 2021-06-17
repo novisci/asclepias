0028ce2e5f5d6562dd0a80b2a1d79912d8319203
Bradley Saul <bsaul@novisci.com>
2021-06-17 14:59:43 +0000

    Resolve "Refactor feature define/eval"

--- test/FeatureComposeSpec.hs
+++ test/FeatureComposeSpec.hs
@@ -1,63 +1,133 @@
 {-# LANGUAGE OverloadedStrings #-}
-
-module FeatureComposeSpec (spec) where
+{-# LANGUAGE DataKinds #-}
+module FeatureComposeSpec (
+ spec
+) where
 
 import FeatureCompose
-import Control.Applicative
 import Test.Hspec ( describe, pending, shouldBe, it, Spec )
-import Data.Foldable
 
-d1 :: FeatureDefinition (FeatureData Int) Int
-d1 = defineM
+
+-----------------------------------
+-- example :: Feature "test" ()
+-- example = MkFeature $ pure ()
+
+-- d0 :: Definition (FeatureData Int)
+-- d0 = define 5
+
+d1 :: Definition (FeatureData Int -> FeatureData Int)
+d1 = defineA
   (\x ->
     if x < 0 then
-      featureDataL $ Other "at least 1 < 0"
+      missingBecause $ Other "at least 1 < 0"
     else
       pure (x + 1)
    )
 
-d2 :: FeatureDefinition (FeatureData Int) Int
+d2 :: Definition (FeatureData Int -> FeatureData Int)
 d2 = define (*2)
 
-d3 ::  FeatureDefinition (FeatureData Int, FeatureData Int) Int
-d3 = define2 (+)
+
+d3 ::  Definition (FeatureData Int -> FeatureData Int -> FeatureData Int)
+d3 = define (+)
+
+f1 :: Int -> Int
+f1 = (+2)
+
+f1D :: Definition (FeatureData Int -> FeatureData Int)
+f1D = define f1
+
+f1F :: Definition (Feature "someInt" Int -> Feature "anotherInt" Int)
+f1F = define f1
+
+f2 :: Bool -> FeatureData Int
+f2 True = pure 1
+f2 False = missingBecause $ Other "test"
+
+f2D :: Definition (FeatureData Bool -> FeatureData Int)
+f2D = defineA f2
+
+f2' :: Bool -> Feature "someInt" Int
+f2' True = pure 1
+f2' False = makeFeature $ missingBecause $ Other "test"
+
+f2F :: Definition (Feature "someBool" Bool -> Feature "someInt" Int)
+f2F = defineA f2'
+
+f3 :: Bool -> Int -> String
+f3 True 1 = "this"
+f3 False 9 = "that"
+f3 _ _ = "otherwise"
+
+f3D :: Definition (FeatureData Bool -> FeatureData Int -> FeatureData String)
+f3D = define f3
+
+f3F :: Definition (Feature "myBool" Bool -> Feature "someInt" Int -> Feature "myString" String)
+f3F = define f3
+
 
 spec :: Spec
 spec = do
 
   describe "checking d1" $
     do
-
-      it "eval of d1 returns correct values for List" $
-        eval d1 (featureDataR 5) `shouldBe`
+      it "eval of d1 on d0" $
+        eval d1 (pure 5) `shouldBe` featureDataR  6
+      it "eval of d1 returns correct value" $
+        eval d1 (featureDataR 5 ) `shouldBe`
           featureDataR 6
-      it "d1 returns correct error for List" $
+      it "d1 returns correct error" $
         eval d1 (featureDataR (-1)) `shouldBe`
-          (featureDataL (Other "at least 1 < 0") :: (FeatureData Int))
+          missingBecause (Other "at least 1 < 0")
 
   describe "checking d2" $
     do
-      it "eval of d2 returns correct values for List" $
+      it "eval of d2 returns correct value" $
         eval d2 (featureDataR 5) `shouldBe`
           featureDataR 10
-      it "eval of d2 returns correct values for List" $
+      it "eval of d2 returns correct value" $
         eval d2 (featureDataR 5) `shouldBe`
           featureDataR 10
-      it "d2 returns correct error for List" $
-        eval d2 (featureDataL (Other "at least 1 < 0")) `shouldBe`
-          (featureDataL (Other "at least 1 < 0") :: (FeatureData Int))
+      it "d2 returns correct error" $
+        eval d2 (featureDataL (Other "at least 1 < 0") ) `shouldBe`
+          featureDataL (Other "at least 1 < 0")
 
   describe "checking d3" $
     do
-      it "eval of d2 returns correct values for List" $
+      it "eval of d2 returns correct values" $
         eval d3 (featureDataR 5, featureDataR 6)  `shouldBe`
           featureDataR 11
-      it "eval of d3 returns correct values for List" $
+      it "eval of d3 returns correct values" $
         eval d3 (featureDataR 5, featureDataR 5) `shouldBe`
           featureDataR 10
-      it "d3 returns correct error for List" $
+      it "d3 returns correct error" $
         eval d3 (featureDataL (Other "at least 1 < 0"), featureDataR 6)  `shouldBe`
-          (featureDataL (Other "at least 1 < 0") :: (FeatureData Int))
-      it "d3 returns correct error for List" $
+          featureDataL (Other "at least 1 < 0")
+      it "d3 returns correct error" $
         eval d3 (featureDataR 6, featureDataL (Other "at least 1 < 0")) `shouldBe`
-          (featureDataL (Other "at least 1 < 0") :: (FeatureData Int))
+          featureDataL (Other "at least 1 < 0")
+
+  describe "checking f1" $
+    do
+      it "eval of f1F on d0 returns correct value" $
+        eval f1F (pure 5) `shouldBe` pure 7
+
+  describe "checking f2" $
+    do
+      it "eval of f2D on d0 returns correct value" $
+        eval f2D (pure True) `shouldBe` pure 1
+      it "eval of f1F on d0 returns correct value" $
+        eval f2D (pure False) `shouldBe` missingBecause (Other "test")
+      it "eval of f1F on d0 returns correct value" $
+        eval f2F (pure False) `shouldBe` makeFeature (missingBecause (Other "test"))
+
+  describe "checking f3" $
+    do
+      it "eval of f3D returns correct value" $
+        eval f3D (pure True, pure 1) `shouldBe` pure "this"
+      it "eval of f3D returns correct value" $
+        eval f3D (pure True, pure 9) `shouldBe` pure "otherwise"
+      it "eval of f3F returns correct value" $
+        eval f3F (pure True, eval f2F (pure False)) `shouldBe` makeFeature (missingBecause (Other "test"))
+      it "eval of f3F  returns correct value" $
+        eval f3F (pure True, eval f2F (pure True)) `shouldBe` pure "this"
--- test/Hasklepias/CohortSpec.hs
+++ test/Hasklepias/CohortSpec.hs
@@ -9,26 +9,25 @@ import FeatureCompose
 import Hasklepias.Cohort
 import Test.Hspec ( describe, pending, shouldBe, it, Spec )
 
-data Feat1
+-- data Feat1
 
-d1 :: FeatureSpec "feat1" () (FeatureData Int) Bool
+d1 :: Definition (Feature "feat" Int -> Feature "feat1" Bool)
 d1 =
-  specifyFeature
-    ()
-    (defineM
+    defineA
     (\x ->
       if x < 0 then
-        featureDataL $ Other "at least 1 < 0"
+        makeFeature $ featureDataL $ Other "at least 1 < 0"
       else
         pure ((x + 1) == 5)
-    ))
+    )
 
 
-d2 :: FeatureSpec "feat2" () (FeatureData Int) Status
-d2 = specifyFeature  () (define (\x -> includeIf (x*2 > 4)))
+d2 :: Definition (Feature "feat" Int -> Feature "feat2" Status)
+d2 = define (\x -> includeIf (x*2 > 4))
 
-d3 :: FeatureSpec "feat3" () (FeatureData Int) Int
-d3 = specifyFeature () (define (+ 2))
+d3 :: Definition (Feature "feat" Int -> Feature  "feat3" Int)
+d3 = define (+ 2)
+
 
 testSubject1 :: Subject Int
 testSubject1 = MkSubject ("1", 0)
@@ -38,26 +37,26 @@ testSubject2 = MkSubject ("2", 54)
 testPopulation :: Population Int
 testPopulation = MkPopulation [testSubject1, testSubject2]
 
-buildCriteria :: Int -> Criteria ()
+buildCriteria :: Int -> Criteria
 buildCriteria dat = criteria $ pure ( criterion feat1 )
-  where feat1 = evalSpec d2 $ pure dat
+  where feat1 = eval d2 $ pure dat
 
-type Features = (Feature "feat1" () Bool, Feature "feat3" () Int)
+type Features = (Feature "feat1" Bool, Feature "feat3" Int)
 buildFeatures :: Int -> Features
 buildFeatures dat  =
-    ( evalSpec d1 input
-    , evalSpec d3 input
+    ( eval d1 input
+    , eval d3 input
     )
     where input = pure dat
 
-testCohort :: CohortSpec () Int Features
+testCohort :: CohortSpec Int Features
 testCohort = specifyCohort buildCriteria buildFeatures
 
 testOut :: Cohort Features
 testOut = MkCohort
   ( MkAttritionInfo [ (ExcludedBy (1, "feat2"), 1), (Included, 1)]
-  , [MkObsUnit ("2", ( makeFeature () (featureDataR False)
-                     , makeFeature () (featureDataR 56))) ])
+  , [MkObsUnit ("2", ( makeFeature (featureDataR False)
+                     , makeFeature (featureDataR 56))) ])
 
 -- evalCohort testCohort testPopulation
 spec :: Spec

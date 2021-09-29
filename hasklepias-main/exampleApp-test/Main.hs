39d2ced8b9dab4b4c570edb49f017ca55033fd60
bsaul <bradleysaul@gmail.com>
2021-09-29 14:44:00 -0400

    tests up and running

--- hasklepias-main/exampleApp-test/Main.hs
+++ hasklepias-main/exampleApp-test/Main.hs
@@ -1,30 +1,31 @@
 {-| Tests of the example application
 -}
 
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main 
   ( main
   ) where
 
+import Hasklepias
 import Hasklepias.ExampleApp
 import Test.Tasty (defaultMain, TestTree, testGroup)
 import Test.Tasty.Silver
 import qualified Data.ByteString.Lazy          as B
 import Hasklepias.MakeApp (runApp)
 
--- exampleAppTest :: IO ()
--- exampleAppTest = do
---   r <- runApp exampleApp "TODO"
---   B.writeFile "test/tests/testrw.json" r
-
+appTestRw :: IO ()
+appTestRw = do
+  r <- runAppWithLocation (Local "exampleApp-test/test/testData.jsonl") exampleAppRW 
+  B.writeFile "exampleApp-test/test/testrw.json" r
 
 tests :: TestTree
 tests = testGroup
-  "Tests of cohort collection"
-  [ 
-    -- goldenVsFile "ExampleApp of row-wise cohort"
-    --              "test/tests/testrw.golden"
-    --              "test/tests/testrw.json"
-    --              exampleAppTest
+  "Tests of exampleApp"
+  [  goldenVsFile "ExampleApp of row-wise cohort"
+                 "exampleApp-test/test/testrw.golden"
+                 "exampleApp-test/test/testrw.json"
+                 appTestRw
 --   , goldenVsFile "Collection of column-wise cohorts"
 --                  "test/tests/testcw.golden"
 --                  "test/tests/testcw.json"

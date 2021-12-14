b260b364cac0308eaaa1a6efa26f40b7c2f33256
bsaul <bradleysaul@gmail.com>
2021-12-13 20:16:33 -0500

    toy around with CI

---
+++ ci/ci-build-docker.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# Build a docker image in the CI.
+#
+# USAGE #
+#
+# The script takes 2 arguments:
+# 1. the version with which to tag the image
+# 2. name of the dockerfile
+
+set -e
+
+echo $CI_REGISTRY_PASSWORD | \
+  docker login \
+  --username $CI_REGISTRY_USER \
+  --password-stdin $CI_REGISTRY
+
+NAME=$CI_REGISTRY_IMAGE/${5:-hasklepias}-build
+
+docker build \
+  --tag $NAME:$1 \
+  --tag $NAME:latest \
+  --build-arg statocker_image=${3:-haskell} \
+  --build-arg ghc_version=${4:-8.10.7} \
+  --file $2 .
+
+docker push $NAME:$1
+docker push $NAME:latest

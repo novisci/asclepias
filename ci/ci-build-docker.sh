688373e173b778f621e93a6237be837d5544d286
bsaul <bradleysaul@gmail.com>
2021-12-14 07:04:00 -0500

    try bash

--- ci/ci-build-docker.sh
+++ ci/ci-build-docker.sh
@@ -25,8 +25,8 @@ echo "using statocker image: ${3}:${4}"
 docker build \
   --tag $NAME:$1 \
   --tag $NAME:latest \
-  --build-arg statocker_image=${3} \
-  --build-arg ghc_version=${4} \
+  --build-arg STATOCKER=${3} \
+  --build-arg GHC=${4} \
   --file $2 .
 
 docker push $NAME:$1

19443ae8cc6166ae8a9bed1438e135e849c73f0c
bsaul <bradleysaul@gmail.com>
2021-12-15 08:28:38 -0500

    get name of app

--- ci/ci-docs-check.sh
+++ ci/ci-docs-check.sh
@@ -1,12 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 # 
 # Run checks on the consistency of the documentation
 set -e
 
 TEMP=$( mktemp -d )
 
+COLLECTORAPP=$( < install/collector.name )
 ## Compare the help text for the cohort collector application
-cabal exec -- cohort-collector --help > ${TEMP}help.txt
+./install/${COLLECTORAPP} --help > ${TEMP}help.txt
 diff ${TEMP}help.txt cohort-collector/help.txt
 
 rm -r ${TEMP}

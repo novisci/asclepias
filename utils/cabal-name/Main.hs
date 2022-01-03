86bd8bd74fe02927db16d593a586c86cda22a879
Bradley Saul <bsaul@novisci.com>
2022-01-03 15:45:06 +0000

    Resolve "CI docker"

--- utils/cabal-name/Main.hs
+++ utils/cabal-name/Main.hs
@@ -1,14 +1,32 @@
 module Main where
 
 import Distribution.Package
+import Distribution.Pretty
 import Distribution.PackageDescription
 import Distribution.PackageDescription.Parsec
 import Distribution.Verbosity
+import Options.Applicative
 import System.Environment
+-- import qualified Text.PrettyPrint as PP
+
+newtype CabalFile = MkCabalFile String
+
+nameApp :: Parser CabalFile
+nameApp = MkCabalFile <$> argument str (metavar "FILE")
+
+opts :: ParserInfo CabalFile
+opts = Options.Applicative.info
+  (nameApp <**> helper)
+  (progDesc "" <> header "cohort collector")
+
+f :: CabalFile -> IO GenericPackageDescription
+f (MkCabalFile x ) = readGenericPackageDescription silent x
+
 main :: IO ()
 main = 
   do 
-    args <- getArgs
-    pkg <- readGenericPackageDescription silent (args !! 1)
-    putStrLn $ show $  packageName $ packageDescription pkg
-    -- putStrLn packageName "Hello, Haskell!"
+    args <- execParser opts
+    pkg <- f args
+    -- putStrLn $ unPackageName $ packageName $ packageDescription pkg
+    print (pretty $  packageName $ packageDescription pkg )
+    -- print (pretty $  packageVersion $ packageDescription pkg )

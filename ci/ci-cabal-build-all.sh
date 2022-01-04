cf5092f7d4ef7c1cfd2328e6604ef329af19aa35
bsaul <bradleysaul@gmail.com>
2022-01-04 09:27:10 -0500

    set haddock to only run on main branch

--- ci/ci-cabal-build-all.sh
+++ ci/ci-cabal-build-all.sh
@@ -8,6 +8,6 @@ cabal build all \
   # --test-show-details=always # only seems to work using cabal test
 
 # If on the main branch then run haddock
-# if [[ "$CI_COMMIT_BRANCH" == "$CI_DEFAULT_BRANCH" ]]; then
+if [[ "$CI_COMMIT_BRANCH" == "$CI_DEFAULT_BRANCH" ]]; then
   ./ci/ci-cabal-haddock-docs.sh
-# fi
+fi

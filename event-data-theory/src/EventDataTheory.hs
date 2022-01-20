825298944ea541ce6a7921fa9de580e2fb1aaf1e
Bradley Saul <bsaul@novisci.com>
2022-01-20 14:55:24 -0500

    removes Predicatable module

--- event-data-theory/src/EventDataTheory.hs
+++ event-data-theory/src/EventDataTheory.hs
@@ -4,8 +4,6 @@
 
 module EventDataTheory
   ( module EventDataTheory.Core
-  , module EventDataTheory.Predicatable
   ) where
 
 import           EventDataTheory.Core
-import           EventDataTheory.Predicatable

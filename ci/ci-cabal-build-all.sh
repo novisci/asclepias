ebf70cfd415a2b4118d7c18c9d417c885cc37e31
David Pritchard <dpritchard@novisci.com>
2022-01-20 10:06:45 -0500

    add --enable-coverage flags to build

--- ci/ci-cabal-build-all.sh
+++ ci/ci-cabal-build-all.sh
@@ -5,11 +5,12 @@ cabal build all \
   -j \
   --enable-tests \
   --enable-benchmarks \
+  --enable-coverage
 
 cabal test all \
   -j \
-  --test-show-details=always
-
+  --test-show-details=always \
+  --enable-coverage
 
 # If on the main branch then run haddock
 if [[ "$CI_COMMIT_BRANCH" == "$CI_DEFAULT_BRANCH" ]]; then

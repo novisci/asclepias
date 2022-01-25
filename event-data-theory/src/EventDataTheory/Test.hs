d863af085d9db87271da8edeb0e1dc99d9c433e2
Bradley Saul <bsaul@novisci.com>
2022-01-25 08:11:04 -0500

    just adding file

---
+++ event-data-theory/src/EventDataTheory/Test.hs
@@ -0,0 +1,11 @@
+{-|
+Module      : Hasklepias Event Tests
+Description : Provides test making functions for event models
+-}
+
+module EventDataTheory.Test () where
+
+import EventDataTheory.Core
+
+foo :: Bool
+foo = True

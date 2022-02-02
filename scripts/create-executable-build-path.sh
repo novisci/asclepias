bec690efe7b4f7fbbd52f354d466eff946a12d8c
David Pritchard <dpritchard@novisci.com>
2022-02-02 14:50:32 -0500

    add assert-exactly-one-line.sh

--- scripts/create-executable-build-path.sh
+++ scripts/create-executable-build-path.sh
@@ -22,19 +22,7 @@ fi
 
 # Find the path to the executable
 paths=$(find dist-newstyle -type f -executable -path "*$PKG*$COMPONENT" -print)
-
-# Ensure that we've found exactly 1 executable path. This assumes that there
-# aren't any newlines in any of the paths, otherwise we will get spurious
-# results
-n_paths=$(printf "%s" "$paths" | grep -c "^")
-if [ "$n_paths" -eq 0 ]; then
-    1>&2 echo "error: couldn't find a path for the executable with package name $PKG and component $COMPONENT"
-    exit 1
-elif [ "$n_paths" -ge 2 ]; then
-    1>&2 echo 'error: found more than one path:'
-    1>&2 echo "$paths"
-    exit 1
-fi
+scripts/assert-exactly-one-line.sh "$paths"
 
 # Print the path to the executable
 echo "$paths"
--- scripts/get-version-from-cabal.sh
+++ scripts/get-version-from-cabal.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
 version=$(grep -e '^version:' "$1" | sed 's/version:[[:space:]]*//g')
-if ! [ $? ]; then
-    1>&2 echo "error: unable to extract the version number from $1"
-    exit 1
-fi
+scripts/assert-exactly-one-line.sh "$version"
 echo "$version"

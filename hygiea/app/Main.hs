da05ccd825dac96e4a8422f9efa6667884055b77
bbrown <bbrown@targetrwe.com>
2022-02-07 10:04:55 -0500

    working example

--- hygiea/app/Main.hs
+++ hygiea/app/Main.hs
@@ -1,16 +1,22 @@
 {-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import qualified Planning.Test as T
+import Planning.Output
 import Planning.Decode
 import Planning.Examples.Simple
 
 main :: IO ()
 main = do
   output <- parseDhallFile "./hygiea/src/Planning/Examples/example.dhall"
-  putStrLn "Parsed from Dhall:" 
+  putStrLn "Parsed from Dhall:"
   print output
   -- TODO: rework Testable a bit
-  --let result = T.testIt inputs output
-  --print result
+  let outcomes = T.toOutput inputs :: [ProjectOutcome]
+  let result = from outcomes :: OutputData
+  putStrLn "Cohort produced:"
+  print result
+  putStrLn "Result of test:"
+  print (result == output)
 

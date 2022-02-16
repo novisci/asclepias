3970971223804b3e30cfab0eb3b59d55b7a3bb34
Bradley Saul <bsaul@novisci.com>
2022-02-16 09:57:36 -0500

    moves stype tests to tasty framework

---
+++ stype/src/Stype/Tests.hs
@@ -0,0 +1,16 @@
+{-|
+Module      : stype package tests
+Description : for internal use
+-}
+module Stype.Tests
+  ( stypeTests
+  ) where
+
+import qualified Stype.Tests.Aeson             as A
+import qualified Stype.Tests.Censored          as C
+import           Test.Tasty
+
+-- | All the tests of stype
+stypeTests :: IO ()
+stypeTests = defaultMain . testGroup "stype package tests" =<< sequenceA
+  [pure A.tests, pure C.tests]
---
+++ stype/test/Main.hs
@@ -0,0 +1,7 @@
+module Main
+  ( main
+  ) where
+
+import           Stype.Tests
+main :: IO ()
+main = stypeTests

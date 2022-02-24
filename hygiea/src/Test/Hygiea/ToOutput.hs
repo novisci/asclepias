d437c4701a69d18348440e93c824852eda6af988
bbrown <bbrown@targetrwe.com>
2022-02-24 09:57:05 -0500

    tooutput docs

--- hygiea/src/Test/Hygiea/ToOutput.hs
+++ hygiea/src/Test/Hygiea/ToOutput.hs
@@ -1,5 +1,14 @@
 {-|
-   -}
+   At a minimum, project programmers will need to specify @ToOutput@ instances for their Hygiea testing routines. This class provides a single method, for transforming project-relevant @input@ to @output@.
+
+   The class also enforces the notion that test routines should be defined by their types: There can be only one @ToOutput@ instance for a given pair of @input@ and @output@. Doing so was a choice to have the type system determine the test as much as possible.
+
+   The rationale is as follows: Hygiea aims to allow non-programmers to provide a direct line to test specification via csv file inputs and ouputs. Project programmers need only specify the schema for the internal inputs and outputs, and a @toOutput@ transformation for getting from one to the other. If Hygiea were to allow more than one transformation per pair of types, there would need to be some additional configuration to allow non-programmers to specify the transformation. Such a configuration could have details that are difficult to resolve and difficult for those who did not write the code to reason about. 
+
+   Of course, if needed programmers could circumvent this intentional limitation by defining newtype wrappers around existing types.
+
+   The @Testable@ constraint alias is one every @input@ and @output@ type to be tested should adhere to. It requires types to be convertible from the internal flat csv-like representation, @TestMap@, and to be instances of @ToOutput@.
+     -}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE ScopedTypeVariables #-}

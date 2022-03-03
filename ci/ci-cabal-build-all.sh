d13812ccbb40a712a463a4c3be26dce060fc3924
David Pritchard <dpritchard@novisci.com>
2022-03-03 17:54:41 -0500

    try to appease the linter

--- ci/ci-cabal-build-all.sh
+++ ci/ci-cabal-build-all.sh
@@ -10,9 +10,10 @@ cabal build all \
 
 # The `exampleAppRW` and `exampleAppCW` applications are required to perform the
 # testing
-cabal install hasklepias-main:exampleAppRW --install-method=copy --installdir=$(pwd)
-cabal install hasklepias-main:exampleAppCW --install-method=copy --installdir=$(pwd)
-export PATH=$(pwd):$PATH
+cabal install hasklepias-main:exampleAppRW --install-method=copy --installdir="$(pwd)"
+cabal install hasklepias-main:exampleAppCW --install-method=copy --installdir="$(pwd)"
+PATH=$(pwd):$PATH
+export PATH
 
 cabal test all \
   -j \

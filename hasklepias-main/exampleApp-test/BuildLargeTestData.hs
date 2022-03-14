b8b09f490762198eac0deb9b0461bfc994a3afe0
David Pritchard <dpritchard@novisci.com>
2022-03-14 15:39:08 -0400

    begin many subject/event golden file construction

--- hasklepias-main/exampleApp-test/BuildLargeTestData.hs
+++ hasklepias-main/exampleApp-test/BuildLargeTestData.hs
@@ -15,13 +15,15 @@ nDigits = length $ show (nReplicates - 1)
 formatNum :: Int -> String
 formatNum = printf ("%0" ++ show nDigits ++ "s") . show
 
+patientRe = mkRegex "\"(a|b|c)\""
+
 updateIds :: Regex -> String -> [String] -> [String]
 updateIds pattern replacement =
   map subRegexPtl where
   subRegexPtl = \line -> subRegex pattern line replacement
 
 constructReplacement :: String -> String
-constructReplacement label = "\"\\1-" ++ label ++ "\""
+constructReplacement label = "\"\1-" ++ label ++ "\""
 
 generateLines ::
      (String -> Bool)
@@ -51,10 +53,32 @@ generateData infilePath outfilePath transform = do
 
 makeManySubjectsTestData :: String -> String -> IO ()
 makeManySubjectsTestData infilepath outfilepath = do
-  generateData infilepath outfilepath (generateNewSubjs $ mkRegex "\"(a|b|c)\"")
+  generateData infilepath outfilepath (generateNewSubjs patientRe)
 
 makeManyEventsTestData :: String -> String -> IO ()
 makeManyEventsTestData infilepath outfilepath = do
   generateData infilepath outfilepath transform where
     checkIfSubjA s = (s !! 2 == 'a') && (s !! 3 == '"')
     transform = generateNewEvents . filter checkIfSubjA
+
+outputStart :: String
+outputStart = "{\"example\":[{\"attritionInfo\":[[{\"tag\":\"SubjectHasNoIndex\"},0],[{\"contents\":[1,\"dummy\"],\"tag\":\"ExcludedBy\"},0],[{\"tag\":\"Included\"},2]],\"totalSubjectsProcessed\":2,\"totalUnitsProcessed\":2},{\"contents\":{\"attributes\":[{\"attrs\":{\"getDerivation\":\"\",\"getLongLabel\":\"another label\",\"getPurpose\":{\"getRole\":[\"Outcome\"],\"getTags\":[]},\"getShortLabel\":\"somelabel\"},\"name\":\"myVar1\",\"type\":\"Count\"},{\"attrs\":{\"getDerivation\":\"\",\"getLongLabel\":\"\",\"getPurpose\":{\"getRole\":[],\"getTags\":[]},\"getShortLabel\":\"\"},\"name\":\"myVar2\",\"type\":\"Bool\"}],\"cohortData\":["
+
+rwPatientEntries :: [String]
+rwPatientEntries =
+  [ "[[\"a\",[\"2010-07-06\",\"2010-07-07\"]],[5,true]]"
+  , "[[\"b\",[\"2010-07-06\",\"2010-07-07\"]],[5,true]]"
+  ]
+
+rwPatientManys :: [String]
+rwPatientManys = generateNewSubjs patientRe rwPatientEntries
+
+cwVarEntries :: [String]
+cwVarEntries =
+  [ "5"
+  , "true"
+  , "[\"a\",[\"2010-07-06\",\"2010-07-07\"]]"
+  ]
+
+outputEnd :: String
+outputEnd = "]},\"tag\":\"RW\"}]}"

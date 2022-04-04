185425558c2c5f0036c38721ccb0b0f825a594b0
David Pritchard <dpritchard@novisci.com>
2022-04-04 19:40:35 -0400

    export more path functions

--- hasklepias-main/exampleApp-test/ConstructPaths.hs
+++ hasklepias-main/exampleApp-test/ConstructPaths.hs
@@ -1,12 +1,23 @@
 -- |
 
 module ConstructPaths
-  ( createFilenameForGolden
+  ( convFilenameToFilepathResult
+  , convFilenameToFilepathTest
+  , convFilenameToS3KeyResult
+  , convFilenameToS3KeyTest
+  , convFilenameToS3UriResult
+  , convFilenameToS3UriTest
+  , createFilenameForGolden
   , createFilenameForResult
   , createFilenameForTest
+  , createFilenameForTestBase
   , createFilepathForGolden
   , createFilepathForResult
   , createFilepathForTest
+  , createS3KeyForResult
+  , createS3KeyForTest
+  , createS3UriForTest
+  , createS3UriForResult
   , localResultsDir
   , localTestDataDir
   , s3Bucket
@@ -27,12 +38,17 @@ s3Bucket = "download.novisci.com"
 s3RootDir :: String
 s3RootDir = "hasklepias/sandbox-testapps/cohortApp/"
 
+-- -- Create the local filepath where the test data is stored
+createFilenameForTestBase :: TestDataType -> String
+createFilenameForTestBase TestDataEmpty = "testEmptyData.jsonl"
+createFilenameForTestBase TestDataSmall = "testData.jsonl"
+createFilenameForTestBase TestDataManySubj = "testmanysubjects.jsonl"
+createFilenameForTestBase TestDataManyEvent = "testmanyevents.jsonl"
+
 -- Create the local filepath where the test data is stored
-createFilenameForTest :: TestDataType -> String
-createFilenameForTest TestDataEmpty = "testEmptyData.jsonl"
-createFilenameForTest TestDataSmall = "testData.jsonl"
-createFilenameForTest TestDataManySubj = "testmanysubjects.jsonl"
-createFilenameForTest TestDataManyEvent = "testmanyevents.jsonl"
+createFilenameForTest :: TestScenarioCohort  -> String
+createFilenameForTest testScenarioCohort =
+  createFilenameForTestBase (getCohortTestDataType testScenarioCohort)
 
 -- Construct the filename for the output for a given test
 createFilenameForResult :: TestScenarioCohort -> String
@@ -74,11 +90,10 @@ createFilenameForGolden testScenarioCohort =
         AppColumnWise -> "cw"
     ++ ".golden"
 
-
 createFilepathForTest ::  TestScenarioCohort -> String
 createFilepathForTest testScenarioCohort =
   localTestDataDir
-    ++ createFilenameForTest (getCohortTestDataType testScenarioCohort)
+    ++ createFilenameForTest testScenarioCohort
 
 -- Helper function to create the local filepath based on a test scenario
 createFilepathForResult :: TestScenarioCohort -> String
@@ -88,3 +103,52 @@ createFilepathForResult testScenarioCohort =
 createFilepathForGolden :: TestScenarioCohort -> String
 createFilepathForGolden testScenarioCohort =
   localTestDataDir ++ createFilenameForGolden testScenarioCohort
+
+createS3KeyForTest :: String -> TestScenarioCohort -> String
+createS3KeyForTest sessionId testScenarioCohort =
+  convFilenameToS3KeyTest
+    sessionId
+    (createFilenameForTest testScenarioCohort)
+
+createS3KeyForResult :: String -> TestScenarioCohort -> String
+createS3KeyForResult sessionId testScenarioCohort =
+  convFilenameToS3KeyResult sessionId (createFilenameForResult testScenarioCohort)
+
+createS3UriForTest :: String -> TestScenarioCohort -> String
+createS3UriForTest sessionId testScenarioCohort =
+  convFilenameToS3UriTest sessionId (createFilenameForTest testScenarioCohort)
+
+createS3UriForResult :: String -> TestScenarioCohort -> String
+createS3UriForResult sessionId testScenarioCohort =
+  convFilenameToS3UriResult sessionId (createFilenameForResult testScenarioCohort)
+
+convFilenameToFilepathTest :: String -> String
+convFilenameToFilepathTest = (localTestDataDir ++)
+
+convFilenameToFilepathResult :: String -> String
+convFilenameToFilepathResult = (localResultsDir ++)
+
+-- Create the S3 key where the test data will be located (once paired with a
+-- bucket)
+convFilenameToS3KeyTest :: String -> String -> String
+convFilenameToS3KeyTest sessionId filename =
+  s3RootDir ++ sessionId ++ "/testdata/" ++ filename
+
+-- Create the S3 key where the results will be located (once paired with a
+-- bucket)
+convFilenameToS3KeyResult :: String -> String -> String
+convFilenameToS3KeyResult sessionId filename =
+  s3RootDir ++ sessionId ++ "/results/" ++ filename
+
+-- Create the S3 key where the test data will be located (once paired with a
+-- bucket)
+convFilenameToS3UriTest :: String -> String -> String
+convFilenameToS3UriTest sessionId filename = convS3KeyToUri $ convFilenameToS3KeyTest sessionId filename
+
+-- Create the S3 key where the test data will be located (once paired with a
+-- bucket)
+convFilenameToS3UriResult :: String -> String -> String
+convFilenameToS3UriResult sessionId filename = convS3KeyToUri $ convFilenameToS3KeyResult sessionId filename
+
+convS3KeyToUri :: String -> String
+convS3KeyToUri s3Key = "s3://" ++ s3Bucket ++ "/" ++ s3Key

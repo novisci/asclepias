7e5edd7ef0d25060e387cdfc75214c6ea200d4d9
David Pritchard <dpritchard@novisci.com>
2022-04-06 15:53:55 -0400

    move path routines into ConstructPaths.hs

--- cohort-collector/test/Main.hs
+++ cohort-collector/test/Main.hs
@@ -6,6 +6,7 @@ module Main
   ) where
 
 import           CohortCollectionTests
+import           ConstructPaths
 import           Control.Exception             ( catch
                                                , throwIO )
 import           System.Directory               ( createDirectoryIfMissing
@@ -49,18 +50,6 @@ main = do
       removeDirectoryRecursive localResultsDir
       s3RecursiveRm  (convS3KeyToUri (s3RootDir ++ sessionId))
       throwIO (e :: ExitCode))
-
-localTestDataDir :: String
-localTestDataDir = "test/tests/"
-
-localResultsDir :: String
-localResultsDir = "test/results/"
-
-s3Bucket :: String
-s3Bucket = "download.novisci.com"
-
-s3RootDir :: String
-s3RootDir = "hasklepias/sandbox-testapps/collectorApp/"
 
 appGoldenVsFile' :: String -> TestCollectorScenario -> TestTree
 appGoldenVsFile' sessionId =
@@ -107,90 +96,6 @@ postCollectorCmdHookS3' sessionId =
   postCollectorCmdHookS3
     (constructS3UriForResult sessionId)
     constructFilepathForResult
-
-constructFilenameForTestBase :: AppType -> FilePath
-constructFilenameForTestBase AppRowWise = "testrw.locations"
-constructFilenameForTestBase AppColumnWise = "testcw.locations"
-
-constructFilenameForTest :: TestCollectorScenario -> FilePath
-constructFilenameForTest =
-  constructFilenameForTestBase . getTestCollectorAppType
-
-constructFilenameForResult :: TestCollectorScenario -> String
-constructFilenameForResult testCollectorScenario =
-  "results-"
-    ++ case getTestCollectorAppType testCollectorScenario of
-        AppRowWise -> "rw"
-        AppColumnWise -> "cw"
-    ++ "-"
-    ++ case getTestCollectorInputType testCollectorScenario of
-        TestCollectorInputFile -> "filein"
-        TestCollectorInputS3 -> "s3in"
-    ++ "-"
-    ++ case getTestCollectorOutputType testCollectorScenario of
-        TestCollectorOutputFile -> "fileout"
-        TestCollectorOutputStdout -> "stdout"
-        TestCollectorOutputS3 -> "s3out"
-    ++ ".json"
-
-constructFilenameForGolden :: TestCollectorScenario -> FilePath
-constructFilenameForGolden testCollectorScenario =
-  case getTestCollectorAppType testCollectorScenario of
-        AppRowWise -> "testrw.golden"
-        AppColumnWise -> "testcw.golden"
-
-constructFilepathForTestBase :: AppType -> FilePath
-constructFilepathForTestBase =
-  (localTestDataDir ++) . constructFilenameForTestBase
-
-constructFilepathForTest :: TestCollectorScenario -> FilePath
-constructFilepathForTest = constructFilepathForTestBase . getTestCollectorAppType
-
-constructFilepathForResult :: TestCollectorScenario -> FilePath
-constructFilepathForResult =
-  (localResultsDir ++) . constructFilenameForResult
-
-constructFilepathForGolden :: TestCollectorScenario -> FilePath
-constructFilepathForGolden =
-  (localTestDataDir ++) . constructFilenameForGolden
-
-constructBucketForTest :: TestCollectorScenario -> String
-constructBucketForTest = const s3Bucket
-
-constructS3KeyForTest :: String -> TestCollectorScenario -> String
-constructS3KeyForTest sessionId testCollectorScenario =
-  convFilenameToS3KeyTest
-    sessionId
-    (constructFilenameForTest testCollectorScenario)
-
-constructS3KeyForResult :: String -> TestCollectorScenario -> String
-constructS3KeyForResult sessionId testCollectorScenario =
-  convFilenameToS3KeyResult
-    sessionId
-    (constructFilenameForResult testCollectorScenario)
-
-constructS3UriForResult :: String -> TestCollectorScenario -> String
-constructS3UriForResult sessionId testCollectorScenario =
-  convS3KeyToUri
-    (constructS3KeyForResult sessionId testCollectorScenario)
-
-convFilenameToFilepathTest :: FilePath -> FilePath
-convFilenameToFilepathTest = (localTestDataDir ++)
-
-convFilenameToS3KeyTest :: String -> String -> String
-convFilenameToS3KeyTest sessionId filename =
-  s3RootDir ++ sessionId ++ "/testdata/" ++ filename
-
-convFilenameToS3UriTest :: String -> String -> String
-convFilenameToS3UriTest sessionId =
-  convS3KeyToUri . convFilenameToS3KeyTest sessionId
-
-convFilenameToS3KeyResult :: String -> String -> String
-convFilenameToS3KeyResult sessionId filename =
-  s3RootDir ++ sessionId ++ "/results/" ++ filename
-
-convS3KeyToUri :: String -> String
-convS3KeyToUri s3Key = "s3://" ++ s3Bucket ++ "/" ++ s3Key
 
 constructCollectorTestName :: TestCollectorScenario -> String
 constructCollectorTestName testCollectorScenario =

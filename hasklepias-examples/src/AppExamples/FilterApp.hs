4e20aa091e8f0a8d95a4895fad717fd880770d78
David Pritchard <dpritchard@novisci.com>
2022-04-07 17:05:53 -0400

    update imports

--- hasklepias-examples/src/AppExamples/FilterApp.hs
+++ hasklepias-examples/src/AppExamples/FilterApp.hs
@@ -3,6 +3,8 @@
 module AppExamples.FilterApp
   ( exampleFilterApp
   ) where
+import           ExampleEvents
+-- import           ExampleEvents                      (  ExampleModel(..) )
 import           Hasklepias
 
 -- | A basic test of the filter app: filter to subject that have at least one enrollment event.
@@ -10,3 +12,10 @@ exampleFilterApp :: FilterApp IO
 exampleFilterApp = makeFilterApp
   "Prefilter test app"
   (getPredicate isEnrollmentEvent :: Event Text ExampleModel Day -> Bool)
+
+isEnrollmentEvent :: Predicate (Event Text ExampleModel Day)
+isEnrollmentEvent = Predicate
+  (\x -> case getFacts (getContext x) of
+    Enrollment -> True
+    _          -> False
+  )

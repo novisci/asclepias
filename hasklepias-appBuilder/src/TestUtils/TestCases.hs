6026515fa54020f9ec7af01094bd73803de92d34
David Pritchard <dpritchard@novisci.com>
2022-04-11 17:42:39 -0400

    uncomment test cases

--- hasklepias-appBuilder/src/TestUtils/TestCases.hs
+++ hasklepias-appBuilder/src/TestUtils/TestCases.hs
@@ -112,77 +112,77 @@ createTestsCartesian :: String -> (TestScenarioCohort -> TestTree) -> TestTree
 createTestsCartesian testName appTest = testGroup
   testName
   [ appTestWrp AppRowWise    TestDataEmpty     TestInputFile  TestOutputFile
-  -- , appTestWrp AppRowWise    TestDataEmpty     TestInputFile  TestOutputStdout
-  -- , appTestWrp AppRowWise    TestDataEmpty     TestInputFile  TestOutputS3
-  -- , appTestWrp AppRowWise    TestDataEmpty     TestInputStdin TestOutputFile
-  -- , appTestWrp AppRowWise    TestDataEmpty     TestInputStdin TestOutputStdout
-  -- , appTestWrp AppRowWise    TestDataEmpty     TestInputStdin TestOutputS3
-  -- , appTestWrp AppRowWise    TestDataEmpty     TestInputS3    TestOutputFile
-  -- , appTestWrp AppRowWise    TestDataEmpty     TestInputS3    TestOutputStdout
-  -- , appTestWrp AppRowWise    TestDataEmpty     TestInputS3    TestOutputS3
+  , appTestWrp AppRowWise    TestDataEmpty     TestInputFile  TestOutputStdout
+  , appTestWrp AppRowWise    TestDataEmpty     TestInputFile  TestOutputS3
+  , appTestWrp AppRowWise    TestDataEmpty     TestInputStdin TestOutputFile
+  , appTestWrp AppRowWise    TestDataEmpty     TestInputStdin TestOutputStdout
+  , appTestWrp AppRowWise    TestDataEmpty     TestInputStdin TestOutputS3
+  , appTestWrp AppRowWise    TestDataEmpty     TestInputS3    TestOutputFile
+  , appTestWrp AppRowWise    TestDataEmpty     TestInputS3    TestOutputStdout
+  , appTestWrp AppRowWise    TestDataEmpty     TestInputS3    TestOutputS3
   , appTestWrp AppRowWise    TestDataSmall     TestInputFile  TestOutputFile
-  -- , appTestWrp AppRowWise    TestDataSmall     TestInputFile  TestOutputStdout
-  -- , appTestWrp AppRowWise    TestDataSmall     TestInputFile  TestOutputS3
-  -- , appTestWrp AppRowWise    TestDataSmall     TestInputStdin TestOutputFile
-  -- , appTestWrp AppRowWise    TestDataSmall     TestInputStdin TestOutputStdout
-  -- , appTestWrp AppRowWise    TestDataSmall     TestInputStdin TestOutputS3
-  -- , appTestWrp AppRowWise    TestDataSmall     TestInputS3    TestOutputFile
-  -- , appTestWrp AppRowWise    TestDataSmall     TestInputS3    TestOutputStdout
-  -- , appTestWrp AppRowWise    TestDataSmall     TestInputS3    TestOutputS3
-  -- , appTestWrp AppRowWise    TestDataManySubj  TestInputFile  TestOutputFile
-  -- , appTestWrp AppRowWise    TestDataManySubj  TestInputFile  TestOutputStdout
-  -- , appTestWrp AppRowWise    TestDataManySubj  TestInputFile  TestOutputS3
-  -- , appTestWrp AppRowWise    TestDataManySubj  TestInputStdin TestOutputFile
-  -- , appTestWrp AppRowWise    TestDataManySubj  TestInputStdin TestOutputStdout
-  -- , appTestWrp AppRowWise    TestDataManySubj  TestInputStdin TestOutputS3
-  -- , appTestWrp AppRowWise    TestDataManySubj  TestInputS3    TestOutputFile
-  -- , appTestWrp AppRowWise    TestDataManySubj  TestInputS3    TestOutputStdout
-  -- , appTestWrp AppRowWise    TestDataManySubj  TestInputS3    TestOutputS3
-  -- , appTestWrp AppRowWise    TestDataManyEvent TestInputFile  TestOutputFile
-  -- , appTestWrp AppRowWise    TestDataManyEvent TestInputFile  TestOutputStdout
-  -- , appTestWrp AppRowWise    TestDataManyEvent TestInputFile  TestOutputS3
-  -- , appTestWrp AppRowWise    TestDataManyEvent TestInputStdin TestOutputFile
-  -- , appTestWrp AppRowWise    TestDataManyEvent TestInputStdin TestOutputStdout
-  -- , appTestWrp AppRowWise    TestDataManyEvent TestInputStdin TestOutputS3
-  -- , appTestWrp AppRowWise    TestDataManyEvent TestInputS3    TestOutputFile
-  -- , appTestWrp AppRowWise    TestDataManyEvent TestInputS3    TestOutputStdout
-  -- , appTestWrp AppRowWise    TestDataManyEvent TestInputS3    TestOutputS3
-  -- , appTestWrp AppColumnWise TestDataEmpty     TestInputFile  TestOutputFile
-  -- , appTestWrp AppColumnWise TestDataEmpty     TestInputFile  TestOutputStdout
-  -- , appTestWrp AppColumnWise TestDataEmpty     TestInputFile  TestOutputS3
-  -- , appTestWrp AppColumnWise TestDataEmpty     TestInputStdin TestOutputFile
-  -- , appTestWrp AppColumnWise TestDataEmpty     TestInputStdin TestOutputStdout
-  -- , appTestWrp AppColumnWise TestDataEmpty     TestInputStdin TestOutputS3
-  -- , appTestWrp AppColumnWise TestDataEmpty     TestInputS3    TestOutputFile
-  -- , appTestWrp AppColumnWise TestDataEmpty     TestInputS3    TestOutputStdout
-  -- , appTestWrp AppColumnWise TestDataEmpty     TestInputS3    TestOutputS3
-  -- , appTestWrp AppColumnWise TestDataSmall     TestInputFile  TestOutputFile
-  -- , appTestWrp AppColumnWise TestDataSmall     TestInputFile  TestOutputStdout
-  -- , appTestWrp AppColumnWise TestDataSmall     TestInputFile  TestOutputS3
-  -- , appTestWrp AppColumnWise TestDataSmall     TestInputStdin TestOutputFile
-  -- , appTestWrp AppColumnWise TestDataSmall     TestInputStdin TestOutputStdout
-  -- , appTestWrp AppColumnWise TestDataSmall     TestInputStdin TestOutputS3
-  -- , appTestWrp AppColumnWise TestDataSmall     TestInputS3    TestOutputFile
-  -- , appTestWrp AppColumnWise TestDataSmall     TestInputS3    TestOutputStdout
-  -- , appTestWrp AppColumnWise TestDataSmall     TestInputS3    TestOutputS3
-  -- , appTestWrp AppColumnWise TestDataManySubj  TestInputFile  TestOutputFile
-  -- , appTestWrp AppColumnWise TestDataManySubj  TestInputFile  TestOutputStdout
-  -- , appTestWrp AppColumnWise TestDataManySubj  TestInputFile  TestOutputS3
-  -- , appTestWrp AppColumnWise TestDataManySubj  TestInputStdin TestOutputFile
-  -- , appTestWrp AppColumnWise TestDataManySubj  TestInputStdin TestOutputStdout
-  -- , appTestWrp AppColumnWise TestDataManySubj  TestInputStdin TestOutputS3
-  -- , appTestWrp AppColumnWise TestDataManySubj  TestInputS3    TestOutputFile
-  -- , appTestWrp AppColumnWise TestDataManySubj  TestInputS3    TestOutputStdout
-  -- , appTestWrp AppColumnWise TestDataManySubj  TestInputS3    TestOutputS3
-  -- , appTestWrp AppColumnWise TestDataManyEvent TestInputFile  TestOutputFile
-  -- , appTestWrp AppColumnWise TestDataManyEvent TestInputFile  TestOutputStdout
-  -- , appTestWrp AppColumnWise TestDataManyEvent TestInputFile  TestOutputS3
-  -- , appTestWrp AppColumnWise TestDataManyEvent TestInputStdin TestOutputFile
-  -- , appTestWrp AppColumnWise TestDataManyEvent TestInputStdin TestOutputStdout
-  -- , appTestWrp AppColumnWise TestDataManyEvent TestInputStdin TestOutputS3
-  -- , appTestWrp AppColumnWise TestDataManyEvent TestInputS3    TestOutputFile
-  -- , appTestWrp AppColumnWise TestDataManyEvent TestInputS3    TestOutputStdout
-  -- , appTestWrp AppColumnWise TestDataManyEvent TestInputS3    TestOutputS3
+  , appTestWrp AppRowWise    TestDataSmall     TestInputFile  TestOutputStdout
+  , appTestWrp AppRowWise    TestDataSmall     TestInputFile  TestOutputS3
+  , appTestWrp AppRowWise    TestDataSmall     TestInputStdin TestOutputFile
+  , appTestWrp AppRowWise    TestDataSmall     TestInputStdin TestOutputStdout
+  , appTestWrp AppRowWise    TestDataSmall     TestInputStdin TestOutputS3
+  , appTestWrp AppRowWise    TestDataSmall     TestInputS3    TestOutputFile
+  , appTestWrp AppRowWise    TestDataSmall     TestInputS3    TestOutputStdout
+  , appTestWrp AppRowWise    TestDataSmall     TestInputS3    TestOutputS3
+  , appTestWrp AppRowWise    TestDataManySubj  TestInputFile  TestOutputFile
+  , appTestWrp AppRowWise    TestDataManySubj  TestInputFile  TestOutputStdout
+  , appTestWrp AppRowWise    TestDataManySubj  TestInputFile  TestOutputS3
+  , appTestWrp AppRowWise    TestDataManySubj  TestInputStdin TestOutputFile
+  , appTestWrp AppRowWise    TestDataManySubj  TestInputStdin TestOutputStdout
+  , appTestWrp AppRowWise    TestDataManySubj  TestInputStdin TestOutputS3
+  , appTestWrp AppRowWise    TestDataManySubj  TestInputS3    TestOutputFile
+  , appTestWrp AppRowWise    TestDataManySubj  TestInputS3    TestOutputStdout
+  , appTestWrp AppRowWise    TestDataManySubj  TestInputS3    TestOutputS3
+  , appTestWrp AppRowWise    TestDataManyEvent TestInputFile  TestOutputFile
+  , appTestWrp AppRowWise    TestDataManyEvent TestInputFile  TestOutputStdout
+  , appTestWrp AppRowWise    TestDataManyEvent TestInputFile  TestOutputS3
+  , appTestWrp AppRowWise    TestDataManyEvent TestInputStdin TestOutputFile
+  , appTestWrp AppRowWise    TestDataManyEvent TestInputStdin TestOutputStdout
+  , appTestWrp AppRowWise    TestDataManyEvent TestInputStdin TestOutputS3
+  , appTestWrp AppRowWise    TestDataManyEvent TestInputS3    TestOutputFile
+  , appTestWrp AppRowWise    TestDataManyEvent TestInputS3    TestOutputStdout
+  , appTestWrp AppRowWise    TestDataManyEvent TestInputS3    TestOutputS3
+  , appTestWrp AppColumnWise TestDataEmpty     TestInputFile  TestOutputFile
+  , appTestWrp AppColumnWise TestDataEmpty     TestInputFile  TestOutputStdout
+  , appTestWrp AppColumnWise TestDataEmpty     TestInputFile  TestOutputS3
+  , appTestWrp AppColumnWise TestDataEmpty     TestInputStdin TestOutputFile
+  , appTestWrp AppColumnWise TestDataEmpty     TestInputStdin TestOutputStdout
+  , appTestWrp AppColumnWise TestDataEmpty     TestInputStdin TestOutputS3
+  , appTestWrp AppColumnWise TestDataEmpty     TestInputS3    TestOutputFile
+  , appTestWrp AppColumnWise TestDataEmpty     TestInputS3    TestOutputStdout
+  , appTestWrp AppColumnWise TestDataEmpty     TestInputS3    TestOutputS3
+  , appTestWrp AppColumnWise TestDataSmall     TestInputFile  TestOutputFile
+  , appTestWrp AppColumnWise TestDataSmall     TestInputFile  TestOutputStdout
+  , appTestWrp AppColumnWise TestDataSmall     TestInputFile  TestOutputS3
+  , appTestWrp AppColumnWise TestDataSmall     TestInputStdin TestOutputFile
+  , appTestWrp AppColumnWise TestDataSmall     TestInputStdin TestOutputStdout
+  , appTestWrp AppColumnWise TestDataSmall     TestInputStdin TestOutputS3
+  , appTestWrp AppColumnWise TestDataSmall     TestInputS3    TestOutputFile
+  , appTestWrp AppColumnWise TestDataSmall     TestInputS3    TestOutputStdout
+  , appTestWrp AppColumnWise TestDataSmall     TestInputS3    TestOutputS3
+  , appTestWrp AppColumnWise TestDataManySubj  TestInputFile  TestOutputFile
+  , appTestWrp AppColumnWise TestDataManySubj  TestInputFile  TestOutputStdout
+  , appTestWrp AppColumnWise TestDataManySubj  TestInputFile  TestOutputS3
+  , appTestWrp AppColumnWise TestDataManySubj  TestInputStdin TestOutputFile
+  , appTestWrp AppColumnWise TestDataManySubj  TestInputStdin TestOutputStdout
+  , appTestWrp AppColumnWise TestDataManySubj  TestInputStdin TestOutputS3
+  , appTestWrp AppColumnWise TestDataManySubj  TestInputS3    TestOutputFile
+  , appTestWrp AppColumnWise TestDataManySubj  TestInputS3    TestOutputStdout
+  , appTestWrp AppColumnWise TestDataManySubj  TestInputS3    TestOutputS3
+  , appTestWrp AppColumnWise TestDataManyEvent TestInputFile  TestOutputFile
+  , appTestWrp AppColumnWise TestDataManyEvent TestInputFile  TestOutputStdout
+  , appTestWrp AppColumnWise TestDataManyEvent TestInputFile  TestOutputS3
+  , appTestWrp AppColumnWise TestDataManyEvent TestInputStdin TestOutputFile
+  , appTestWrp AppColumnWise TestDataManyEvent TestInputStdin TestOutputStdout
+  , appTestWrp AppColumnWise TestDataManyEvent TestInputStdin TestOutputS3
+  , appTestWrp AppColumnWise TestDataManyEvent TestInputS3    TestOutputFile
+  , appTestWrp AppColumnWise TestDataManyEvent TestInputS3    TestOutputStdout
+  , appTestWrp AppColumnWise TestDataManyEvent TestInputS3    TestOutputS3
   ]
   where
     appTestWrp appType testDataType testInputType testOutputType =
@@ -194,15 +194,15 @@ createCollectorTests testName appTest = testGroup
   [ appTestWrp AppRowWise    TestCollectorInputFile TestCollectorOutputFile
   , appTestWrp AppRowWise    TestCollectorInputFile TestCollectorOutputStdout
   , appTestWrp AppRowWise    TestCollectorInputFile TestCollectorOutputS3
-  -- , appTestWrp AppRowWise    TestCollectorInputS3   TestCollectorOutputFile
-  -- , appTestWrp AppRowWise    TestCollectorInputS3   TestCollectorOutputStdout
-  -- , appTestWrp AppRowWise    TestCollectorInputS3   TestCollectorOutputS3
+  , appTestWrp AppRowWise    TestCollectorInputS3   TestCollectorOutputFile
+  , appTestWrp AppRowWise    TestCollectorInputS3   TestCollectorOutputStdout
+  , appTestWrp AppRowWise    TestCollectorInputS3   TestCollectorOutputS3
   , appTestWrp AppColumnWise TestCollectorInputFile TestCollectorOutputFile
   , appTestWrp AppColumnWise TestCollectorInputFile TestCollectorOutputStdout
   , appTestWrp AppColumnWise TestCollectorInputFile TestCollectorOutputS3
-  -- , appTestWrp AppColumnWise TestCollectorInputS3   TestCollectorOutputFile
-  -- , appTestWrp AppColumnWise TestCollectorInputS3   TestCollectorOutputStdout
-  -- , appTestWrp AppColumnWise TestCollectorInputS3   TestCollectorOutputS3
+  , appTestWrp AppColumnWise TestCollectorInputS3   TestCollectorOutputFile
+  , appTestWrp AppColumnWise TestCollectorInputS3   TestCollectorOutputStdout
+  , appTestWrp AppColumnWise TestCollectorInputS3   TestCollectorOutputS3
   ]
   where
     appTestWrp appType inputType outputType =

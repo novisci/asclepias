19616fdbdee1232308cf4391b43736ddb89e5e4a
bbrown <bbrown@targetrwe.com>
2022-05-17 16:08:47 -0400

    top-level exports

---
+++ monarch/src/Monarch.hs
@@ -0,0 +1,43 @@
+module Monarch
+  ( RoutineContext
+  , TestRoutine(..)
+  , RoutineElem(..)
+  , monarchTest
+  -- | Types and classes
+  -- See Test.Monarch.TestMap for Map utilities
+  , MonarchException
+  , TestMap
+  , TestVal(..)
+  , TestAtomic(..)
+  , Atomizable
+  , Testable
+  , ToOutput(..)
+  -- | Parsers
+  , tryParseRecordsCsv
+  , decodeMapSchema
+  , decodeMapSchemaAuto
+  , parseDhallFile
+  , parseDhallFileWith
+  -- | Reexports
+  -- Conversion instances to derive via Generic if
+  -- needed for custom types
+  , Generic
+  , ToDhall
+  , FromDhall
+  , ToJSON
+  , FromJSON
+  ) where
+
+import           Test.Monarch.MonarchException
+import           Test.Monarch.Parse
+import           Test.Monarch.TestMap
+import           Test.Monarch.ToOutput
+import           Test.Tasty.Monarch
+
+import           Data.Aeson                     ( FromJSON
+                                                , ToJSON
+                                                )
+import           Dhall                          ( FromDhall
+                                                , ToDhall
+                                                )
+import           GHC.Generics                   ( Generic )

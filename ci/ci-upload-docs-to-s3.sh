d0260a894389fc159a0e1af60d034e4207affe57
Bradley Saul <bsaul@novisci.com>
2022-07-08 14:59:33 -0400

    remove profile that I needed locally

--- ci/ci-upload-docs-to-s3.sh
+++ ci/ci-upload-docs-to-s3.sh
@@ -6,7 +6,6 @@ aws s3 sync \
 		"${ARTIFACT_DIR}" \
 		s3://docs.novisci.com/asclepias/api/ \
 		--delete --acl public-read \
-		--profile novisci
 
 aws cloudfront create-invalidation \
 	--distribution-id E171A73GHZQS0B \

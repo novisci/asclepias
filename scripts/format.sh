af3e9d8120660276536257cc1c0a05822b239822
Bradley Saul <bsaul@novisci.com>
2022-08-11 12:01:41 -0400

    update format script

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-# Formats haskell source code files in place using brittany source code formatter.
+# Formats haskell source code files in place using stylish-haskell source code formatter.
 
-# Check and see if `brittany` is installed so as to give users a useful message
-# if they don't know what brittany is or how to install it
-if ! [ -x "$(command -v brittany)" ]; then
-  echo 'Error: cannot find '\''brittany'\'' application' >&2
-  echo 'Use the command '\''cabal install brittany'\'' to install the application'  >&2
+# Check and see if `stylish-haskell` is installed so as to give users a useful message
+# if they don't know what stylish-haskell is or how to install it
+if ! [ -x "$(command -v stylish-haskell)" ]; then
+  echo 'Error: cannot find '\''stylish-haskell'\'' application' >&2
+  echo 'Use the command '\''cabal install stylish-haskell'\'' to install the application'  >&2
   exit 1
 fi
-find . -name "*.hs" -not -path './dist-newstyle/*' -exec sh -c 'brittany "$1" --write-mode=inplace' shell {} \;
+find . -name "*.hs" -not -path './dist-newstyle/*' -exec sh -c 'stylish-haskell "$1" --i./scnplace' shell {} \;

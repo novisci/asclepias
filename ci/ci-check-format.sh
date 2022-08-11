784ae420128ec486e6858433728946c537d18b58
Bradley Saul <bsaul@novisci.com>
2022-08-11 10:25:19 -0400

    update CI script

--- ci/ci-check-format.sh
+++ ci/ci-check-format.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
-# Checks that the source code is formatted using the brittany
-# formatter (https://hackage.haskell.org/package/brittany)
+# Checks that the source code is formatted using the stylish-haskell
+# formatter (https://hackage.haskell.org/package/stylish-haskell)
 set -e 
 
-find . -name "*.hs" -not -path './dist-newstyle/*' -print0 |
-  xargs -0 brittany -c
+if find . -name "*.hs" -not -path './dist-newstyle/*' -print0 |
+  xargs -0 stylish-haskell --config ci/ci-stylish-haskell.yaml 2>&1 |
+  grep '^'; then
+echo ""
+echo "formatting changes required. Run ./scripts/format.sh"
+exit 1
+else 
+exit 0
+fi

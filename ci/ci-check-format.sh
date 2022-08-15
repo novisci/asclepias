8a507a428aaafe2440f4163bc09827b5cb98bc81
Bradley Saul <bsaul@novisci.com>
2022-08-15 10:19:06 -0400

    try this

--- ci/ci-check-format.sh
+++ ci/ci-check-format.sh
@@ -3,16 +3,5 @@
 # formatter (https://hackage.haskell.org/package/stylish-haskell)
 set -e 
 
-# find . -name "*.hs" -not -path './dist-newstyle/*' -print0 |
-#   xargs -0 stylish-haskell --config ci/ci-stylish-haskell.yaml
-
-
-if find . -name "*.hs" -not -path './dist-newstyle/*' -print0 |
-  xargs -0 stylish-haskell --config ci/ci-stylish-haskell.yaml 2>&1 |
-  grep '^'; then
-echo ""
-echo "formatting changes required. Run ./scripts/format.sh"
-exit 1
-else 
-exit 0
-fi
+find . -name "*.hs" -not -path './dist-newstyle/*' -print0 |
+  xargs -0 stylish-haskell -r --config ci/ci-stylish-haskell.yaml

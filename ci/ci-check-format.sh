f4a5ddb1912a0004327bdd614fba5322c9477f91
Bradley Saul <bsaul@novisci.com>
2022-08-15 10:14:48 -0400

    update ci format script

--- ci/ci-check-format.sh
+++ ci/ci-check-format.sh
@@ -3,5 +3,16 @@
 # formatter (https://hackage.haskell.org/package/stylish-haskell)
 set -e 
 
-find . -name "*.hs" -not -path './dist-newstyle/*' -print0 |
-  xargs -0 stylish-haskell --config ci/ci-stylish-haskell.yaml
+# find . -name "*.hs" -not -path './dist-newstyle/*' -print0 |
+#   xargs -0 stylish-haskell --config ci/ci-stylish-haskell.yaml
+
+
+if find . -name "*.hs" -not -path './dist-newstyle/*' -print0 |
+  xargs -0 stylish-haskell --config ci/ci-stylish-haskell.yaml 2>&1 |
+  grep '^'; then
+echo ""
+echo "formatting changes required. Run ./scripts/format.sh"
+exit 1
+else 
+exit 0
+fi

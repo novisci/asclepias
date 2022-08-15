1578a4216fc0c99b63773a59c281480af3007eae
Bradley Saul <bsaul@novisci.com>
2022-08-15 10:26:18 -0400

    try this

--- ci/ci-check-format.sh
+++ ci/ci-check-format.sh
@@ -3,7 +3,10 @@
 # formatter (https://hackage.haskell.org/package/stylish-haskell)
 set -e 
 
-stylish-haskell --version
+# stylish-haskell --version
 
-find . -name "*.hs" -not -path './dist-newstyle/*' -print0 |
-  xargs -0 stylish-haskell --recursive --config ci/ci-stylish-haskell.yaml
+# find . -name "*.hs" -not -path './dist-newstyle/*' -print0 |
+#   xargs -0 stylish-haskell --recursive --config ci/ci-stylish-haskell.yaml
+
+
+curl -sL https://raw.github.com/haskell/stylish-haskell/master/scripts/latest.sh | sh -s --recursive --config ci/ci-stylish-haskell.yaml 

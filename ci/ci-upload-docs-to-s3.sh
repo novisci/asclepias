aa3877b0e1d77f6aa21e7c6f96d61df26d2650fd
Brendan Brown <bbrown@targetrwe.com>
2023-04-19 20:28:27 +0000

    Resolve "ci-upload-docs-to-s3.sh silently fails"

--- ci/ci-upload-docs-to-s3.sh
+++ ci/ci-upload-docs-to-s3.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-ARTIFACT_DIR=install/docs
+#ARTIFACT_DIR=install/docs
+
+set -e
+
+echo "ARTIFACT_DIR=$ARTIFACT_DIR"
 
 aws s3 sync \
-		"${ARTIFACT_DIR}" \
-		s3://docs.novisci.com/asclepias/api/ \
-		--delete --acl public-read \
+	"${ARTIFACT_DIR}" \
+	s3://docs.novisci.com/asclepias/api/ \
+	--delete --acl public-read
+
+echo "exit code from aws s3 sync: $?"
 
 aws cloudfront create-invalidation \
 	--distribution-id E171A73GHZQS0B \
